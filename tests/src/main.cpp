f7511dc0058201c847a34b7687561f39921e0eb2
martinberlin <martin@luckycloud.de>
2019-09-30 21:37:19 +0200

    Probably will need help with BrotliEncoderCompress

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,14 +1,19 @@
 extern "C" {
-#include "brotli/decode.h"
+#include <brotli/decode.h>
+#include <brotli/encode.h>
 }
 #include "Arduino.h"
 #include "FS.h"
 #include "SPIFFS.h"
 
+#define DEFAULT_LGWIN 24
+#define COMPRESSION_BUFFER 2000;
+size_t fileSize;
+
 void setup() {
   Serial.begin(115200);
    
-  long time = millis();
+  int time = millis();
   uint8_t compressed[] = {27, 175, 4,248, 141, 148, 110, 222, 68, 85, 134, 214, 32, 33, 108, 111, 106, 22, 199, 106, 129, 12, 168, 102, 47, 4};
  
   uint8_t buffer [2000];
@@ -22,36 +27,54 @@ void setup() {
     buffer);
  
   Serial.printf("%.*s\n", output_length, buffer);
-  long timespent = millis()-time;
+  int timespent = millis()-time;
   Serial.printf("Decompression took %d milliseconds.", timespent);
-
-
 
   String fileName = "/144.txt";
 
   Serial.println("Reading file '"+fileName+"' on SPIFFS");
-  char *readBuffer = new char[2000];
+  char *inBuffer = new char[2000];
 
  if (!SPIFFS.begin()) {
    Serial.println("Could not mount file system");
  }
   if (SPIFFS.exists(fileName)) {
-    File file = SPIFFS.open(fileName, "r");
-    int fileSize = file.size(); 
-    
-    file.readBytes(readBuffer, fileSize);
-    Serial.printf("%d bytes read into readBuffer", fileSize);
-
+    File file = SPIFFS.open(fileName, "r"); 
+    fileSize = file.size();
+    file.readBytes(inBuffer, fileSize);
     file.close();
+    Serial.printf("%d bytes read into inBuffer", fileSize);
   } else {
     Serial.println("Could not read "+fileName+" from SPIFFS");
     return;
   }
+
+
+  int quality = 9; /* 1 less to 9 max. compression */
+  size_t encodedSize;
+  uint8_t outBuffer[3000];
+  int lgwin = DEFAULT_LGWIN;
+  size_t input_size = sizeof(inBuffer);
+
+Serial.printf("%d bytes input_size", input_size);
+bool returnValue = false;
+
+//***ERROR*** A stack overflow in task loopTask has been detected.
+//abort() was called at PC 0x4008b980 on core 1
+
+/* returnValue = BrotliEncoderCompress(
+      quality,  
+      lgwin, 
+      BrotliEncoderMode(BROTLI_MODE_GENERIC),
+      fileSize, 
+      (const uint8_t *)inBuffer,     
+      &encodedSize,
+      outBuffer);  */
   
-  
+  Serial.println(returnValue);
 
 }
  
 void loop() {
-  delay(199);
+  delay(1);
 }
