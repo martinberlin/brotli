1166dcc855916e0a885f68efc6a2614e1d6126f8
martinberlin <martin@luckycloud.de>
2019-09-30 19:31:07 +0200

    Prepare grounds to load text file from SPIFFs

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -2,7 +2,9 @@ extern "C" {
 #include "brotli/decode.h"
 }
 #include "Arduino.h"
- 
+#include "FS.h"
+#include "SPIFFS.h"
+
 void setup() {
   Serial.begin(115200);
    
@@ -19,9 +21,35 @@ void setup() {
     &output_length,
     buffer);
  
- Serial.printf("%.*s\n", output_length, buffer);
- long timespent = millis()-time;
- Serial.printf("Decompression took %d milliseconds.", timespent);
+  Serial.printf("%.*s\n", output_length, buffer);
+  long timespent = millis()-time;
+  Serial.printf("Decompression took %d milliseconds.", timespent);
+
+
+
+  String fileName = "/144.txt";
+
+  Serial.println("Reading file '"+fileName+"' on SPIFFS");
+  char *readBuffer = new char[2000];
+
+ if (!SPIFFS.begin()) {
+   Serial.println("Could not mount file system");
+ }
+  if (SPIFFS.exists(fileName)) {
+    File file = SPIFFS.open(fileName, "r");
+    int fileSize = file.size(); 
+    
+    file.readBytes(readBuffer, fileSize);
+    Serial.printf("%d bytes read into readBuffer", fileSize);
+
+    file.close();
+  } else {
+    Serial.println("Could not read "+fileName+" from SPIFFS");
+    return;
+  }
+  
+  
+
 }
  
 void loop() {
