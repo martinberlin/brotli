7d75432e48d377df983136be81292731b5bafadb
martinberlin <martin@luckycloud.de>
2019-09-30 23:00:52 +0200

    Revert "Still cannot make BrotliEncoderCompress work"
    This reverts commit 1f4a277959a83ae45f7980a1b9bd920b703d486d.

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -8,7 +8,7 @@ extern "C" {
 
 #define DEFAULT_LGWIN 24
 #define COMPRESSION_BUFFER 2000;
-int fileSize;
+size_t fileSize;
 
 void setup() {
   Serial.begin(115200);
@@ -17,7 +17,7 @@ void setup() {
   uint8_t compressed[] = {27, 175, 4,248, 141, 148, 110, 222, 68, 85, 134, 214, 32, 33, 108, 111, 106, 22, 199, 106, 129, 12, 168, 102, 47, 4};
  
   uint8_t buffer [2000];
-  //uint8_t buffer  = new uint8_t[2000]; // For some reason is not the same
+ 
   size_t output_length = sizeof(buffer);
  
   BrotliDecoderDecompress(
@@ -27,8 +27,6 @@ void setup() {
     buffer);
  
   Serial.printf("%.*s\n", output_length, buffer);
-  delete buffer;
-
   int timespent = millis()-time;
   Serial.printf("Decompression took %d milliseconds.", timespent);
 
@@ -50,35 +48,29 @@ void setup() {
     Serial.println("Could not read "+fileName+" from SPIFFS");
     return;
   }
-  
-  // Print out inBuffer to serial
-    for ( int i = 0; i < fileSize; i++ ) {
-        uint8_t conv = (int) inBuffer[i];
-        Serial.print(conv);Serial.print(",");
-    }
 
-  int quality = 1; /* 1 less to 9 max. compression */
+
+  int quality = 9; /* 1 less to 9 max. compression */
   size_t encodedSize;
+  uint8_t outBuffer[3000];
+  int lgwin = DEFAULT_LGWIN;
+  size_t input_size = sizeof(inBuffer);
 
-  uint8_t outBuffer [1000];
-  int lgwin = 19;
-
-Serial.printf("%d bytes input_size", fileSize);
+Serial.printf("%d bytes input_size", input_size);
 bool returnValue = false;
 
 //***ERROR*** A stack overflow in task loopTask has been detected.
 //abort() was called at PC 0x4008b980 on core 1
 
- returnValue = BrotliEncoderCompress(
+/* returnValue = BrotliEncoderCompress(
       quality,  
       lgwin, 
-      BrotliEncoderMode::BROTLI_MODE_GENERIC,
+      BrotliEncoderMode(BROTLI_MODE_GENERIC),
       fileSize, 
       (const uint8_t *)inBuffer,     
       &encodedSize,
-      outBuffer);  
-  delete inBuffer;
-
+      outBuffer);  */
+  
   Serial.println(returnValue);
 
 }
