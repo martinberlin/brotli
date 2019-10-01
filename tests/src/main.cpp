371654ed5c6bb2d5a95791bbb6c67ae0e76753b0
martinberlin <martin@luckycloud.de>
2019-10-01 21:14:32 +0200

    Test decompressing different files from FS

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -12,41 +12,76 @@ size_t fileSize;
 
 File file;
 
-void setup() {
-  Serial.begin(115200);
-   
-  if (!SPIFFS.begin()) {
-   Serial.println("Could not mount file system");
- }
+bool decompressFile(String fileName, bool outputBytes=false) {
+  if (!SPIFFS.exists(fileName)) {
+    Serial.println(fileName+ " not found. Did you execute: pio run -t uploadfs");
+    return false;
+  }
+  bool brotliStatus = true;
+  int readFsTime = millis();
+  file = SPIFFS.open(fileName, "r"); 
+  fileSize = file.size();
+  char *inBuffer = new char[fileSize];
+  file.readBytes(inBuffer, fileSize);
+  file.close();
+  Serial.println("- - - - - - - - - - - - - - - - - - - - - - - - - -");
+  Serial.printf("%d bytes read into inBuffer from: ", fileSize);
+  Serial.print(fileName+ "\n");
 
-    String fileName = "/144-rgbw-2-on.bin.br";
-    file = SPIFFS.open(fileName, "r"); 
-    fileSize = file.size();
-    char *inBuffer = new char[fileSize];
-    file.readBytes(inBuffer, fileSize);
-    file.close();
-    Serial.printf("%d bytes read into inBuffer\n", fileSize);
-
-  int time = millis();
+  int decompressTime = millis();
   // Uncomment for easy test withouth SPIFFS
   //uint8_t inBuffer[] = {27, 175, 4,248, 141, 148, 110, 222, 68, 85, 134, 214, 32, 33, 108, 111, 106, 22, 199, 106, 129, 12, 168, 102, 47, 4};
-  uint8_t buffer [2000];
+  
+  uint8_t buffer[2000];
   size_t output_length = sizeof(buffer);
 
-  BrotliDecoderDecompress(
+  brotliStatus = BrotliDecoderDecompress(
     fileSize,
     (const uint8_t *)inBuffer,
     &output_length,
     buffer);
+  
+  delete(inBuffer);
+
+  int timespent = millis()-decompressTime;
+
+  Serial.printf("Decompression took %d milliseconds. File read took: %d milliseconds\n", timespent, decompressTime-readFsTime);
+  Serial.printf("%d bytes after decompression. Estimated pixels RGBw: %d\n", output_length, (output_length-5)/4);
+  if (outputBytes) {
+    for ( int i = 0; i < output_length; i++ ) {
+      uint8_t conv = (int) buffer[i];
+      Serial.print(conv);Serial.print(",");
+    }
+    Serial.printf("%.*s\n", output_length, buffer);
+  }
+  return brotliStatus;
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  if (!SPIFFS.begin()) {
+   Serial.println("Could not mount file system");
+ }
+
+  decompressFile("/144-rgbw-2-on.bin.br", true);
+  decompressFile("/144-rgbw-all-on.bin.br");
+  decompressFile("/288-rgbw-2-on.bin.br");
+  decompressFile("/288-rgbw-all-on.bin.br");
+  decompressFile("/432-rgbw-2-on.bin.br");
+  decompressFile("/432-rgbw-all-on.bin.br");
+  decompressFile("/864-rgbw-2-on.bin.br");
+  decompressFile("/864-rgbw-all-on.bin.br");
+  decompressFile("/864-rgbw-all-random.bin.br");
+}
  
-  Serial.printf("%.*s\n", output_length, buffer);
+void loop() {
+  delay(1);
+}
 
-  int timespent = millis()-time;
-
-  Serial.printf("%d bytes after decompression\n", output_length);
-  Serial.printf("Decompression took %d milliseconds\n", timespent);
-/*
-  String fileName = "/144.txt";
+void compressFile(String fileName) {
+  // Failed compression (Still need to FIX)
 
   Serial.println("Reading file '"+fileName+"' on SPIFFS");
   char *inBuffer = new char[2000];
@@ -57,7 +92,9 @@ void setup() {
     fileSize = file.size();
     file.readBytes(inBuffer, fileSize);
     file.close();
-    Serial.printf("%d bytes read into inBuffer", fileSize);
+    Serial.println("- - - - - - - - - - - - - - - - - - - - - - - - - -");
+    Serial.printf("%d bytes read into inBuffer from: ", fileSize);
+    Serial.print(fileName+ "\n");
   } else {
     Serial.println("Could not read "+fileName+" from SPIFFS");
     return;
@@ -73,7 +110,6 @@ void setup() {
 
 Serial.printf("%d bytes input_size", input_size);
 bool returnValue = false;
- */
 
 //***ERROR*** A stack overflow in task loopTask has been detected.
 //abort() was called at PC 0x4008b980 on core 1
@@ -86,10 +122,4 @@ bool returnValue = false;
       (const uint8_t *)inBuffer,     
       &encodedSize,
       outBuffer);  */
-  
-
-}
- 
-void loop() {
-  delay(1);
 }
